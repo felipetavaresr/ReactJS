233a6f64335c2b87a3e341f684594502fbc7a6c6
Felipe Tavares <felipe.tavaresk@gmail.com>
2021-06-01 13:41:47 -0300

    😉removing propriety children and projects menu

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import Header from './components/Header';
 function App() {
     return (
     <>
-        <Header title="Homepage">
-            <ul>
-                <li>Homepage</li>
-                <li>About me</li>
-                <li>Contact</li>
-            </ul>
-        </Header>
-        <Header title="Projects"> 
-            <ul>
-                <li>Project 1</li>
-                <li>Project 2</li>
-                <li>Project 3</li>
-            </ul>
-        </Header>
+        <Header title="Homepage" />
     </> 
     );
 }
