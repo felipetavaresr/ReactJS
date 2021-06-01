147b898e229ee28bd299961518805a08c365a69c
Felipe Tavares <felipe.tavaresk@gmail.com>
2021-06-01 13:35:18 -0300

    😀first propriety

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+    <>
+        <Header title="Homepage">
+            <ul>
+                <li>Homepage</li>
+                <li>About me</li>
+                <li>Contact</li>
+            </ul>
+        </Header>
+        <Header title="Projects"> 
+            <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+            </ul>
+        </Header>
+    </> 
+    );
 }
 
 export default App;
